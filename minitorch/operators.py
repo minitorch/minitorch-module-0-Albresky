5f2f2dcf9677bb5d21b0b977201f7c1b49113020
Albresky <albre02@outlook.com>
2024-11-10 14:25:46 +0800

    Implement Task0_1

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -12,11 +12,14 @@ from typing import Callable, Iterable
 # - mul
 # - id
 # - add
+# - addLists
 # - neg
+# - negList
 # - lt
 # - eq
 # - max
 # - is_close
+# - prod
 # - sigmoid
 # - relu
 # - log
@@ -33,6 +36,63 @@ from typing import Callable, Iterable
 
 
 # TODO: Implement for Task 0.1.
+
+def mul(x, y):
+    return x*y
+
+def id(x):
+    return x
+
+def add(x, y):
+    return x+y
+
+def addLists(x, y):
+    return [a+b for a, b in zip(x, y)]
+
+def neg(x):
+    return -x
+
+def negList(x):
+    return [-a for a in x]
+
+def lt(x, y):
+    return x < y
+
+def eq(x, y):
+    return x == y
+
+def max(x, y):
+    return x if x > y else y
+
+def prod(x):
+    return math.prod(x)
+
+def is_close(x, y):
+    return abs(x-y) < 1e-2
+
+def sigmoid(x):
+    return 1.0 / (1.0 + math.exp(-x)) if x >= 0 else math.exp(x) / (1.0 + math.exp(x))
+
+def relu(x):
+    return max(0, x)
+
+def log(x):
+    return math.log(x)
+
+def exp(x):
+    return math.exp(x)
+
+def log_back(x):
+    return 1/x
+
+def inv(x):
+    return 1/x
+
+def inv_back(x):
+    return -1/(x**2)
+
+def relu_back(x, y):
+    return y if x > 0 else 0
 
 
 # ## Task 0.3
